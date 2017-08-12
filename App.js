936d76fde8328a15a509a66a637849b5af57e74a
Xavier Via <fernando.via@gmail.com>
2017-08-13 01:11:55 +0200

    Reset

--- App.js
+++ App.js
@@ -1,25 +1,51 @@
 import React from 'react'
-import { View, TouchableWithoutFeedback } from './ReactDreamNative'
-import withChild from './withChild'
+import { Text, View, TouchableWithoutFeedback, Dimensions } from 'react-native'
+import { Motion, spring } from 'react-motion'
 
-const Box = View.style(({ x, y }) => ({
-  backgroundColor: 'red',
-  width: 100,
-  height: 100,
-  transform: [{ translate: [x - 50, y - 50] }],
-}))
+export default class App extends React.Component {
+  constructor() {
+    super()
 
-const BigView = View.style(() => ({
-  flex: 1,
-  backgroundColor: 'green',
-}))
-  .map(withChild(({ x, y }) => ({ x, y })))
-  .ap(Box)
+    const windowSize = Dimensions.get('window')
 
-const App = TouchableWithoutFeedback.addProps(() => ({
-  onPress: event => console.log(event.nativeEvent),
-}))
-  .map(withChild(({ x, y }) => ({ x, y })))
-  .ap(BigView)
+    this.state = {
+      x: windowSize.width / 2,
+      y: windowSize.height / 2,
+    }
 
-export default () => <App.Component x={100} y={100} />
+    this.handlePress = this.handlePress.bind(this)
+  }
+
+  handlePress(event) {
+    console.log(event.nativeEvent)
+    this.setState({
+      x: event.nativeEvent.pageX,
+      y: event.nativeEvent.pageY,
+    })
+  }
+
+  render() {
+    return (
+      <Motion style={{ x: spring(this.state.x), y: spring(this.state.y) }}>
+        {({ x, y }) =>
+          <TouchableWithoutFeedback onPress={this.handlePress}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: '#fff',
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: 'red',
+                  width: 100,
+                  height: 100,
+                  transform: [{ translate: [x - 50, y - 50] }],
+                }}
+              />
+            </View>
+          </TouchableWithoutFeedback>}
+      </Motion>
+    )
+  }
+}
