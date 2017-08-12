f5e7464c87e8343516ce195529720231613e3d4e
Xavier Via <fernando.via@gmail.com>
2017-08-12 23:11:54 +0200

    Demo application ready

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  Dimensions,
+} from 'react-native'
+import { Motion, spring } from 'react-motion'
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+
+    const windowSize = Dimensions.get('window')
+
+    this.state = {
+      x: windowSize.width / 2,
+      y: windowSize.height / 2,
+      windowSize,
+    }
+
+    this.handlePress = this.handlePress.bind(this)
+  }
+
+  handlePress(event) {
+    console.log(event.nativeEvent)
+    this.setState({
+      x: event.nativeEvent.pageX,
+      y: event.nativeEvent.pageY,
+    })
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <Motion style={{ x: spring(this.state.x), y: spring(this.state.y) }}>
+        {({ x, y }) =>
+          <TouchableWithoutFeedback onPress={this.handlePress}>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: '#fff',
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: 'red',
+                  width: 100,
+                  height: 100,
+                  transform: [{ translate: [x - 50, y - 50] }],
+                }}
+              />
+            </View>
+          </TouchableWithoutFeedback>}
+      </Motion>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
